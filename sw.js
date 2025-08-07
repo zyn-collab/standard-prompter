e150e20e9c61f81fb530e4c2a063525b50472819
zyn-collab <110053616+zyn-collab@users.noreply.github.com>
2025-08-08 00:54:23 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'standard-prompter-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html' // Assuming you rename your main file to index.html
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
